c3c74aa094c227bfefc93797d64a07d194b87b4d
Artem Androsenko <tyoma.and@gmail.com>
2023-06-11 10:52:01 +0300

    Add Card component and fix App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import styles from './App.module.css';
+
+import Card from './components/Card';
 
 function App() {
   return (
-    <div>
-
+    <div className={styles.App}>
+      <Card>
+      </Card>
     </div>
   );
 }

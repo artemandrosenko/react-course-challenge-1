a0f34d8c699dbeceef74ce9c1aa9ed33c6bfeed8
Artem Androsenko <tyoma.and@gmail.com>
2023-06-12 20:59:25 +0300

    Add CreateUser component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import styles from './App.module.css';
 
 import Card from './components/Card';
+import CreateUser from './components/CreateUser';
 
 function App() {
   return (
     <div className={styles.App}>
       <Card>
+        <CreateUser />
       </Card>
     </div>
   );
---
+++ src/components/CreateUser.js
@@ -0,0 +1,36 @@
+import styles from './CreateUser.module.css';
+import React, { useState } from 'react';
+
+const CreateUser = () => {
+
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+
+    const nameHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    const ageHandler = (event) => {
+        setAge(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <form className={`${styles['create-user__form']}`} onSubmit={submitHandler}>
+            <div>
+                <label>Name:</label>
+                <input type='text' placeholder='Enter name here' onChange={nameHandler}/>
+                <label>Age:</label>
+                <input type='number' placeholder='Enter age' onChange={ageHandler} />
+                <button type='submit'>Add new user</button>
+            </div>
+
+        </form>
+    )
+
+}
+
+export default CreateUser;
